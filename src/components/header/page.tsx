3c353f49649b1029c185165563db3afbd3af0f01
Hugo Simões <hugosimoescontato@gmail.com>
2024-08-16 20:19:10 +0000

    navigation and banner-hero responsiveness

--- src/components/header/page.tsx
+++ src/components/header/page.tsx
@@ -3,11 +3,11 @@ import Navigation from "./navigation";
 
 export default function Header() {
   return (
-    <header className="px-6 py-6 flex justify-between items-center">
+    <header className="px-6 py-6 xl:px-20 flex justify-between items-center">
       <img
         src="/logo.svg"
         alt="BDM Logo"
-        className="w-36 h-6 md:w-44 md:h-10"
+        className="w-36 h-6 md:w-44 md:h-10 2xl:w-52"
       />
       <img
         src="/assets/menu.svg"
--- src/components/hero-banner.tsx
+++ src/components/hero-banner.tsx
@@ -1,35 +1,39 @@
 import React from "react";
-import { Button } from "./ui/button";
 import Link from "next/link";
 
 export default function HeroBanner() {
   return (
     <section id="home">
-      <div className="text-center flex flex-col items-center gap-6 mt-8 px-7">
-        <h1 className="text-2xl leading-7 font-bold text-dark-gray">
-          Soluções de{" "}
-          <span className="text-yellow">
-            Engenharia e Segurança do Trabalho
-          </span>
-        </h1>
-        <p className="text-sm text-dark-gray">
-          Somos uma empresa com mais de 35 anos de experiência em engenharia,
-          automação e segurança do trabalho, oferecendo soluções personalizadas
-          e eficientes para diversas indústrias, sempre focados na otimização de
-          processos e na segurança.
-        </p>
-        <Link
-          href=""
-          className="leading-5 bg-yellow p-4 flex w-fit rounded-lg text-white gap-4 font-bold"
-        >
-          Fale conosco
-          <img
-            src="/assets/icons/right-arrow.svg"
-            alt="Icone: Flecha para direita"
-          />
-        </Link>
+      <img src="/caminhao.svg" alt="Caminhao SVG" />
+      <div className="lg:flex items-center xl:-mt-56 2xl:-mt-72">
+        <div className="text-center lg:text-start flex flex-col items-center lg:items-start gap-6 mt-8 px-7 xl:px-16 2xl:pl-40">
+          <h1 className="text-2xl lg:text-3xl xl:text-5xl leading-7 font-bold text-dark-gray">
+            Soluções de{" "}
+            <span className="text-yellow">
+              Engenharia e Segurança do Trabalho.
+            </span>
+          </h1>
+          <p className="text-sm xl:text-lg text-dark-gray">
+            Somos uma empresa com mais de 35 anos de experiência em engenharia,
+            automação e segurança do trabalho, oferecendo soluções
+            personalizadas e eficientes para diversas indústrias, sempre focados
+            na otimização de processos e na segurança.
+          </p>
+          <Link
+            // TODO: Colocar o link do whatsapp com mensagem.
+            href=""
+            className="leading-5 bg-yellow p-4 flex w-fit lg:w-[50%] justify-center rounded-lg text-white gap-4 font-bold"
+          >
+            <span className="xl:hidden">Fale conosco</span>
+            <span className="hidden xl:flex">Entre em contato conosco</span>
+            <img
+              src="/assets/icons/right-arrow.svg"
+              alt="Icone: Flecha para direita"
+            />
+          </Link>
+        </div>
+        <img src="/assets/banner.png" alt="Banner BDM" className="lg:w-[40%]" />
       </div>
-      <img src="/assets/banner.png" alt="Banner BDM" />
     </section>
   );
 }
