9560d5d161001954288d7a0908a3314876bdf469
Hugo Simões <hugosimoescontato@gmail.com>
2024-08-19 13:49:43 +0000

    results responsiviness completed

--- src/components/results.tsx
+++ src/components/results.tsx
@@ -1,42 +1,19 @@
 import React from "react";
-
-const data = [
-  {
-    img: "/assets/clients.svg",
-    number: "250+",
-    title: "Clientes felizes",
-    alt: "SVG de Trabalhadores",
-  },
-  {
-    img: "/assets/projects.svg",
-    number: "600+",
-    title: "Projetos conclúidos",
-    alt: "SVG de Capacete",
-  },
-  {
-    img: "/assets/risk.svg",
-    number: "150+",
-    title: "Análises de risco RN12",
-    alt: "SVG de Capacete",
-  },
-  {
-    img: "/assets/diagnostic.svg",
-    number: "200+",
-    title: "Diagnósticos de serviços em altura NR35",
-    alt: "SVG de Folha de Tarefas",
-  },
-];
+import { resultsData } from "@/constants/results-data";
 
 export default function Results() {
   return (
     <section className="bg-[#05172D] py-14 text-center">
-      <h2 className="text-lg font-bold leading-5 text-yellow mb-8">
+      <h2 className="text-lg lg:text-xl font-bold leading-5 text-yellow mb-8">
         Resultados que falam por nós
       </h2>
-      <div className="grid grid-cols-2 justify-center items-center px-6 gap-8">
-        {data.map((item) => {
+      <div className="grid grid-cols-2 xl:grid-cols-4 justify-center items-center px-6 gap-8">
+        {resultsData.map((item) => {
           return (
-            <div className="flex flex-col items-center gap-4" key={item.title}>
+            <div
+              className="flex flex-col xl:flex-row items-center gap-4"
+              key={item.title}
+            >
               <img src={item.img} alt={item.alt} />
               <div className="flex flex-col items-center text-white text-center">
                 <h3 className="text-xl font-bold leading-normal">
---
+++ src/constants/results-data.ts
@@ -0,0 +1,26 @@
+export const resultsData = [
+  {
+    img: "/assets/clients.svg",
+    number: "250+",
+    title: "Clientes felizes",
+    alt: "SVG de Trabalhadores",
+  },
+  {
+    img: "/assets/projects.svg",
+    number: "600+",
+    title: "Projetos conclúidos",
+    alt: "SVG de Capacete",
+  },
+  {
+    img: "/assets/risk.svg",
+    number: "150+",
+    title: "Análises de risco RN12",
+    alt: "SVG de Capacete",
+  },
+  {
+    img: "/assets/diagnostic.svg",
+    number: "200+",
+    title: "Diagnósticos de serviços em altura NR35",
+    alt: "SVG de Folha de Tarefas",
+  },
+];
